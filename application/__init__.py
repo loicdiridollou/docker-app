bdf9345d90ca6f79e37e5682ff5591ed5a994a5f
Loic Diridollou <loic.diridollou@gmail.com>
2021-09-06 20:51:21 -0700

    Testing build failure

--- application/__init__.py
+++ application/__init__.py
@@ -10,6 +10,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='dev')
 
+    
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
